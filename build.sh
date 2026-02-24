914022e207635af813887d02ede5fbfbfe5ba9eb
thanuja.m <thanujathanu691@gmail.com>
2026-02-24 13:34:32 +0530

    Add build script for Render deployment

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Build script for Render deployment
+
+set -e
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Build complete!"
